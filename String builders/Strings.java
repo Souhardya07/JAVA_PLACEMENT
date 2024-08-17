d56774a4e930183e73dd41d4b3fd348cb235b697
SOCODE <149923296+Souhardya07@users.noreply.github.com>
2024-08-17 11:12:17 +0530

    Create Strings.java

---
+++ String builders/Strings.java
@@ -0,0 +1,24 @@
+public class Strings {
+    public static void main(String args[]){
+        StringBuilder sb = new StringBuilder("Tony");
+            System.out.println(sb);
+
+            // Finding out a charecter at aspecific index
+            
+            System.out.println(sb.charAt(2));
+
+           // Setting a charecter st index 3
+           sb.setCharAt(3,'o');
+           System.out.println(sb);
+
+           // Inserting charecter 
+
+           sb.insert(2, 'n');
+           System.out.println(sb);
+
+           // deleting charecter
+
+           sb.delete(2, 3);
+           System.out.println(sb);
+    }
+}
