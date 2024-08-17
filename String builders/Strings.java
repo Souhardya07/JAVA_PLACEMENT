067f723a6f3c11c17927da1f2016dfee9c695e39
SOCODE <149923296+Souhardya07@users.noreply.github.com>
2024-08-17 12:51:47 +0530

    Update Strings.java

--- String builders/Strings.java
+++ String builders/Strings.java
@@ -1,24 +1,27 @@
 public class Strings {
     public static void main(String args[]){
         StringBuilder sb = new StringBuilder("Tony");
-            System.out.println(sb);
+        //     System.out.println(sb);
 
-            // Finding out a charecter at aspecific index
+        //     // Finding out a charecter at aspecific index
             
-            System.out.println(sb.charAt(2));
+        //     System.out.println(sb.charAt(2));
 
-           // Setting a charecter st index 3
-           sb.setCharAt(3,'o');
-           System.out.println(sb);
+        //    // Setting a charecter st index 3
+        //    sb.setCharAt(3,'o');
+        //    System.out.println(sb);
 
-           // Inserting charecter 
+        //    // Inserting charecter 
 
-           sb.insert(2, 'n');
-           System.out.println(sb);
+        //    sb.insert(2, 'n');
+        //    System.out.println(sb);
 
-           // deleting charecter
+        //    // deleting charecter
 
-           sb.delete(2, 3);
+        //    sb.delete(2, 3);
+        //    System.out.println(sb);
+        //    // append chareccter
+           sb.append("e");
            System.out.println(sb);
     }
 }
