a5cb7c9f5783c4175682d96bdb9a8046400db42f
SOCODE <149923296+Souhardya07@users.noreply.github.com>
2024-08-17 13:44:34 +0530

    Create Arrays.java

---
+++ Arrays/Arrays.java
@@ -0,0 +1,53 @@
+
+//                    Arrays
+
+
+
+// public class Arrays {
+    
+//     public static void main(String args[]){
+//         int [] marks = new int[3];
+//         marks [0] = 97;
+//         marks [1] = 94; 
+//         marks [2] = 91; 
+
+//         for (int i=0;i<3; i++ ){
+//         System.out.println(marks[i]);
+//         }
+//     }
+// }
+
+
+
+//                                                            2D arrays
+ 
+// import java.util.Scanner;
+ 
+//  class Arrays{
+//     public static void main(String[] args) {
+//         Scanner a = new Scanner(System.in);
+//         int rows = a.nextInt();
+//         int cols = a.nextInt();
+
+//         int [][] Number = new int[rows][cols];
+   
+//         for(int i=0; i<rows;i++){
+//             for (int j=0;j<cols;j++){
+                 
+//                 Number[i][j] = a.nextInt();
+
+
+//             }
+//         }
+//         int x = a.nextInt();
+//         for(int i=0; i<rows;i++){
+//             for (int j=0;j<cols;j++){ 
+//                  if (x==Number[i][j]){
+//              System.out.println("The Index of x is : "+ i + j);
+//                  }
+//             }
+//             System.out.println();
+//         }
+        
+//     }
+//  }
