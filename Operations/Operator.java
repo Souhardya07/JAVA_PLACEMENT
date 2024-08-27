4126dc202caf9bc44007b6a62375edfd37946bd0
SOCODE <149923296+Souhardya07@users.noreply.github.com>
2024-08-27 10:29:09 +0530

    Create Operator.java

---
+++ Operations/Operator.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+public class Operator {
+    public static void main(String args[]){
+        // int a=40;
+        // int b = 50;
+        // a = ++a;              // a = 41
+        
+        // System.out.println(a); // The a variable is getting incremented first and then getting printed and also stored in the same variable a.
+        
+        // b = --a;             // a = 40
+        // System.out. println(b); //The variable a is containing the incremented value 41 in it so when we perform --a then it decrements the variable a to back to 40 and printing the same 
+          
+          //BIT MANIPULATION
+
+          // GET BIT
+
+          // int n = 1;
+          // int pos = 2;
+          // int bitmask = 1<< pos;
+          // if((bitmask & n)==0){
+          //   System.out.println("Bit was 0");
+          // }else{
+          //   System.out.println("Bit was 1");
+          // }
+          
+           // SET BIT
+
+
+          // int n = 5;
+          // int pos = 1;
+          // int bitmask = 1<< pos;
+          //  int  newNumber = bitmask |n;
+          //   System.out.println(newNumber);
+          
+          //Output: 7
+
+           // CLEAR BIT
+
+          //  int n = 5;
+          //  int pos = 2;
+          //  int bitmask = 1<<pos;
+          //  int notbitmask = ~(bitmask);
+
+          //  int newNumber = notbitmask & n;
+          //  System.out.println(newNumber);
+
+            // Output : 1
+
+           // Update bit 
+  
+           Scanner sc = new Scanner(System.in);
+           int oper =sc.nextInt();
+           int n = 5;
+           int pos = 1; 
+           int bitmask = 1<<pos;
+           if (oper == 1){
+           int newNumber= bitmask |n;
+           System.out.println(newNumber);
+           }else{
+            int newBitmask = ~(bitmask);
+            int newNumber = newBitmask & n;
+            System.out.println(newNumber);
+           }
+
+    }
+}
