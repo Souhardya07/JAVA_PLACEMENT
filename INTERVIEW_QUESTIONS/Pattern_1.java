e0f359d6813319a59a4f48b3166aee37dc9333a1
SOCODE <149923296+Souhardya07@users.noreply.github.com>
2024-08-29 21:52:53 +0530

    Update Pattern_1.java

--- INTERVIEW_QUESTIONS/Pattern_1.java
+++ INTERVIEW_QUESTIONS/Pattern_1.java
@@ -5,11 +5,46 @@ public class Pattern_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int rows = sc.nextInt();
-        for (int i =1 ; i<=rows;i++){
-            for(int j=1 ; j<=i ;j++){
-               System.out.print(j);
+        // for (int i =1 ; i<=rows;i++){
+        //     for(int j=1 ; j<=i ;j++){
+        //        System.out.print(j);
+        //     }
+        //     System.out.println(" ");
+        // }
+
+        // for(int i = 1; i<=rows;i++){
+        //     for(int j=rows; j>=i;--j){
+        //         if(i<j)
+        //         System.out.print(j);
+        //     }
+        //     System.out.println(" ");
+        // }
+
+        // output 65432
+                //6543
+                //654
+                //65
+                //6
+
+        // for (int i =rows ; i>=1 ;i--){
+        //     for (int j = 1 ; j<i;++j){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println(" ");
+        // }
+        
+        // Output
+        //            12345
+        //            1234
+        //            123
+        //            12
+        //            1
+
+        for (int i = rows ; i >=1 ; i--){
+            for(int j = i ; j >=1 ; j--){
+                System.out.print(j);
             }
-            System.out.println(" ");
+                System.out.println(" ");
         }
     }
 }
