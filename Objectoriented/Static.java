fdb4715c2094ec6a003965ebe6c06890a8d29255
SOCODE <149923296+Souhardya07@users.noreply.github.com>
2024-09-01 20:37:28 +0530

    Create Static.java

---
+++ Objectoriented/Static.java
@@ -0,0 +1,19 @@
+// package Objectoriented;
+
+
+//  class Static {
+//     static int a =3;
+//     static int b = 4;
+//     static void meth(int x){
+//         System.out.println(x);
+//         System.out.println(a);
+//         System.out.println(b);
+//     }
+//     static{
+//         System.out.println("block");
+//         b = a*4;
+//     }
+//     public static void main(String[] args) {
+//         meth(4);
+//     }
+// }
