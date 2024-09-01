13c2caea3283334636ffb566ed369566bd7b6829
SOCODE <149923296+Souhardya07@users.noreply.github.com>
2024-09-01 20:24:59 +0530

    Update Val.java

--- Objectoriented/Val.java
+++ Objectoriented/Val.java
@@ -1,25 +1,26 @@
-package Objectoriented;
-class Test{
-    void meth(int i , int j){
-        i=i*2;
-        j=j/2;
-    }
-}
-public class Val {
-    public static void main(String[] args) {
-        Test ob = new Test();
-        int a =15;
-        int b = 45;
-        System.out.println("before calling ");
-        System.out.println(a);
-        System.out.println(b);
-        ob.meth(a,b);
-        System.out.println("after calling");
-        System.out.println(ob.i);
-        System.out.println(ob.j);
-    }
-}
-/*This particular problem showcases the differnece between the call by
- * reference and call by value 
- * Here as this example is of call by value thus after calling the 
- */
+// package Objectoriented;
+// class Test{
+//     void meth(int i , int j){
+//         i=i*2;
+//         j=j/2;
+//     }
+// }
+// public class Val {
+//     public static void main(String[] args) {
+//         Test ob = new Test();
+//         int a =15;
+//         int b = 45;
+//         System.out.println("before calling ");
+//         System.out.println(a);
+//         System.out.println(b);
+//         ob.meth(a,b);
+//         System.out.println("after calling");
+//         System.out.println(a);
+//         System.out.println(b);
+//     }
+// }
+// /*This particular problem showcases the differnece between the call by
+//  * reference and call by value 
+//  * Here as this example is of call by value thus after calling the 
+//  * the value printed are same.
+//  */
