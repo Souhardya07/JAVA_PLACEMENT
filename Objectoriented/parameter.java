564e11e9afcba137c4316a81da73d4acb7522e07
SOCODE <149923296+Souhardya07@users.noreply.github.com>
2024-09-01 20:25:16 +0530

    Create parameter.java

---
+++ Objectoriented/parameter.java
@@ -0,0 +1,34 @@
+// package Objectoriented;
+
+// class Test{
+//     int a;
+//     int b;
+//     Test(int x, int y){
+//         a = x;
+//         b = y;
+//     }
+//     void meth(Test ob){
+//         ob.a = ob.a*2;
+//         ob.b = ob.b/2;
+//     }
+// }
+
+// class parameter {
+// public static void main(String[] args) {
+//     Test ob = new Test(2,3);
+//     System.out.println(ob.a);
+//     System.out.println(ob.b);
+//     /* OUTPUT :
+//     2
+//     3 
+//     */
+//     ob.meth(ob);
+//     System.out.println(ob.a);
+//     System.out.println(ob.b);
+//     /*OUTPUT:
+//          4
+//          1
+//          as the object elements is called 
+//      */
+// }
+// }
