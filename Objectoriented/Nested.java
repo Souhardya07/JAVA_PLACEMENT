f74963ecb2b9703ecff181289df2d7b772d9f404
SOCODE <149923296+Souhardya07@users.noreply.github.com>
2024-09-01 20:38:47 +0530

    Update Nested.java

--- Objectoriented/Nested.java
+++ Objectoriented/Nested.java
@@ -1,20 +1,24 @@
-package Objectoriented;
-class A{
-    int outer_x=100 ;
-    void test(){
-        B ob = new B();
-        ob.display();
+// package Objectoriented;
+// class A{
+//     int outer_x=100 ;
+//     void test(){
+//         B ob = new B();
+//         ob.display();
 
-    }
-    class B{
-        void display(){
-            System.out.println(outer_x);
-        }
-    }
-}
- class Nested {
-    public static void main(String[] args) {
-        A ob1 = new A();
-        ob1.test();
-    }
-}
+//     }
+//     class B{
+//         void display(){
+//             System.out.println(outer_x);
+//         }
+//     }
+// }
+//  class Nested {
+//     public static void main(String[] args) {
+//         A ob1 = new A();
+//         ob1.test();
+//     }
+// }
+
+//NESTED CLASS EXAMPLE 
+// OUTPUT
+// 100 
