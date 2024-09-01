4f9cd3eea461b1f4583f4722a6537742174ad665
SOCODE <149923296+Souhardya07@users.noreply.github.com>
2024-09-01 20:37:32 +0530

    Update returntype.java

--- Objectoriented/returntype.java
+++ Objectoriented/returntype.java
@@ -1,24 +1,26 @@
-package Objectoriented;
+// package Objectoriented;
 
 
 
-class Test{
-    int a;
-    Test(int i){
-        a =i;
-    }
-    Test incbyten(){
-         Test temp = new Test(a+10);
-         return temp;
-    }
-}
-/* */
-public class returntype {
-    public static void main(String[] args) {
-        Test ob = new Test(2);
-        System.out.println(ob.a);
-        Test ob1 = ob.incbyten();
-        System.out.println(ob1.a);
+// class Test{
+//     int a;
+//     Test(int i){
+//         a =i;
+//     }
+//     Test incbyten(){
+//          Test temp = new Test(a+10);
+//          return temp;
+//     }
+// }
+// /*THIS IS THE WAY IN WHICH 
+//   the object iss used as a return type 
+// */
+// public class returntype {
+//     public static void main(String[] args) {
+//         Test ob = new Test(2);
+//         System.out.println(ob.a);
+//         Test ob1 = ob.incbyten();
+//         System.out.println(ob1.a);
 
-    }
-}
+//     }
+// }
