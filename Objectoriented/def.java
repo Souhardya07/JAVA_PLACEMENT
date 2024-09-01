b75fc3c6b358532a1ea286a01eb3879af53b7a8a
SOCODE <149923296+Souhardya07@users.noreply.github.com>
2024-09-01 18:13:53 +0530

    Update def.java

--- Objectoriented/def.java
+++ Objectoriented/def.java
@@ -1,29 +1,39 @@
-package Objectoriented;
-class Box{
-    double h;
-    double w;
-    double d;
-    Box( double height,double width,double depth){
-       h =height;
-       w = width;
-       d = depth;
-    }
-    Box(double length){
-        h = length;
-        w = length;
-        d = length;
-    }
-    void volume(){
-        System.out.println(h*w*d);
-    }
-}
-class def {
-    public static void main(String[] args) {
-        Box ob = new Box(2,3,4);
-        /*The constructor containing the parameters (There are three parameters here )  */
-        Box ob1 = new Box(6);
-        /*the constructor box with the single parameter is assigned for the object ob */
-        ob.volume();
-        ob1.volume();     
-    }
-}
+
+//CONSTRUCTION OVERLOADING 
+
+    // package Objectoriented;
+    // class Box{
+    //     double h;
+    //     double w;
+    //     double d;
+    //     Box( double height,double width,double depth){
+    //        h =height;
+    //        w = width;
+    //        d = depth;
+    //     }
+    //     Box(double length){
+    //         h = length;
+    //         w = length;
+    //         d = length;
+    //     }
+    //     void volume(){
+    //         System.out.println(h*w*d);
+    //     }
+    // }
+    // class def {
+    //     public static void main(String[] args) {
+    //         Box ob = new Box(2,3,4);
+    //         /*The constructor containing the parameters (There are three parameters here )  */
+    //         Box ob1 = new Box(6);
+    //         /*the constructor box with the single parameter is assigned for the object ob */
+    //         ob.volume();
+    //         ob1.volume();     // Constructor overloading cannot take place with only one object 
+
+    //         /*THIS IS THE ERROR IF WE TRY TO ASSIGN THE SAME OBJECT ob DIFFERENT CONSTRUCTOR
+            
+    //         Exception in thread "main" java.lang.Error: Unresolved compilation problem: 
+    //         Duplicate local variable ob
+
+    //         at Objectoriented.def.main(def.java:24)*/
+    //     }
+    // }
