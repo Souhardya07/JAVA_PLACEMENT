1f44cf6660508a68675c79d5b50aaa203387d4ea
SOCODE <149923296+Souhardya07@users.noreply.github.com>
2024-09-01 17:46:25 +0530

    Create Box.java

---
+++ Objectoriented/Box.java
@@ -0,0 +1,19 @@
+//package Objectoriented;
+
+// public class Box{
+//    public static void main(String[] args) {                               // This was for code one where we are calling for the execution of the same with only one class
+//       //  double height =10;
+//       //  double width = 20;
+//       //  double depth = 50;
+
+//       //   System.out.println("HEIGHT"+ height);                     // This is a normal program showing how the simple pritn statement takes
+//       //   System.out.println("Width"+width);                        // places in the java programming language.
+//       //   System.out.println("Depth"+depth);
+
+
+
+
+//    } 
+ 
+
+// }
