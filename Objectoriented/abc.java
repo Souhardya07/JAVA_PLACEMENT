fa7e54bfb3c86fc512c545527c21743dfca50340
SOCODE <149923296+Souhardya07@users.noreply.github.com>
2024-09-01 17:46:31 +0530

    Create abc.java

---
+++ Objectoriented/abc.java
@@ -0,0 +1,26 @@
+// package Objectoriented;
+// class Box {
+//         double height , width , depth;
+//     }
+//  class abc {
+//     public static void main(String[] args) {
+//         Box ob = new Box ();
+//         /*In this particular example the 
+//         class Box consists of the data members only like 
+//         height , width , depth and in the class abc we 
+//         call the Box class and make an instance of it named
+//         ob (object).
+
+//         -> also we dynamically alloted the memory for the ob 
+//         and accordingly assigned the height,width and depth of the Box class.
+//          */
+//         ob.width = 60;
+//         ob.depth = 50;
+//         double vol = (ob.height*ob.width*ob.depth);           
+//         System.out.println(ob.height);
+//         System.out.println(ob.width);
+//         System.out.println(ob.depth);
+//         System.out.println("the volume is :"+vol);
+//     }
+
+//}
