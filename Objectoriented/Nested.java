751cc9bad5c32c61ec0a21e0acdc3d6221db6fc6
SOCODE <149923296+Souhardya07@users.noreply.github.com>
2024-09-01 20:37:37 +0530

    Create Nested.java

---
+++ Objectoriented/Nested.java
@@ -0,0 +1,20 @@
+package Objectoriented;
+class A{
+    int outer_x=100 ;
+    void test(){
+        B ob = new B();
+        ob.display();
+
+    }
+    class B{
+        void display(){
+            System.out.println(outer_x);
+        }
+    }
+}
+ class Nested {
+    public static void main(String[] args) {
+        A ob1 = new A();
+        ob1.test();
+    }
+}
